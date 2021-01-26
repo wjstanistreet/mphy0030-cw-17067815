c7ba1398a8c47a6eb83d3803b15d02c1c7eedcd8
Will Stanistreet <zcemwjs@ucl.ac.uk>
2021-01-27 01:50:22 +0800

    Added simple_image_read.m

--- part1/task1/simple_image_read.m
+++ part1/task1/simple_image_read.m
@@ -1,15 +1,26 @@
-function imageBin = simple_image_read(image)
+function imgData = simple_image_read(imgBi, vol, voxdims)
 % Reads a 3D medical image to binary file.
 % 
-% simple_image_write writes a 3D image to a binary file, imgName,
-% containing the intensity values, vol, and the voxel dimenions, voxdim.
+% simple_image_read reads  the intensity values, vol, and the voxel 
+% dimenions, voxdim, of a binary file.
 % 
-% % --- Inputs ---
+% --- Inputs ---
 % imgName   - Name of the image you are writing to.
 % vol       - Intensity values of the volume
 % voxdim    - Voxel dimensions of the image
+%
+% --- Outputs ---
+% imgData   - 2D array containing intensity values and voxel dimensions.
 
-opIm = fopen('example_image.mat'); %CHANGE TO IMAGE
-A = fread(opIm);
+% Opens binary file imgBi
+imOpen = fopen(imgBi);
 
+% Reads the intensity values and dimensions of binary file, imgBi.
+imgVal = fread(imOpen, vol, 'int16');
+imgDims = fread(imOpen, voxdims, 'float32');
+
+% Stores the intensity values and dimensions in 2D array, imgData.
+imgData = [imgVal, imgDims];
+
+fclose(imOpen);
 end 
