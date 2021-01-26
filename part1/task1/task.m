5f7acefaca9b49c1dc91d485542fbc5b010d32ca
Will Stanistreet <zcemwjs@ucl.ac.uk>
2021-01-27 02:00:16 +0800

    Addressed second bullet point task1

--- part1/task1/task.m
+++ part1/task1/task.m
@@ -1,3 +1,7 @@
 
 % Loads the example image data
 load('data\example_image.mat');
+
+% Writes the example image data into binary file 
+imgName = ('data\image.sim');
+simple_image_write(imgName, vol, voxdims);
