ba36239ed659d06864e1c0b9bf709fa2202a28e2
Will Stanistreet <zcemwjs@ucl.ac.uk>
2021-01-31 05:20:23 +0800

    Edited but incomplete

--- part1/task2/task.m
+++ part1/task2/task.m
@@ -10,3 +10,13 @@ covMat = cov(x);
 % Creating a multivariate Gaussian probability distribution
 gausspdf = gaussian_pdf(x, meanVec, covMat);
 
+% Finding the 10th, 50th and 90th percentiles
+prc10 = prctile(gausspdf,10);
+prc50 = prctile(gausspdf,50);
+prc90 = prctile(gausspdf,90);
+
+% Plotting the elipsoids:
+
+
+
+
