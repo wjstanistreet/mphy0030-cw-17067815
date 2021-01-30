190e3ce0fbe9f250e279b7273b805e6e8823c434
Will Stanistreet <zcemwjs@ucl.ac.uk>
2021-01-30 21:10:04 +0800

    Added Gradient Descent Function

---
+++ part1/task3/gradient_descent.m
@@ -0,0 +1,23 @@
+function optiVal = gradient_descent(mvf, iniVal, stpSize, maxIter, stpTol, grdFun)
+%  
+% 
+% gradient_descent iteratively finds the minimum value of a multivariate
+% function, mvf, using an intial value of the function, iniVal, with a step
+% size, stpSize,  a maximum number of iterations, maxIter, and a tolerance
+% of the function between steps, stpTol,
+% 
+% --- Inputs ---
+% 
+% mvf       - Multivariate input function 
+% iniVal    - Initial value of the input function
+% stpSize   - Step size of the gradient descent
+% maxIter   - Maximum number of iterations
+% stpTol    - Tolerance of function between subsequent steps 
+% grdFun    - Gradiet function of the input multivariate function
+% 
+% --- Output ---
+% 
+% optiVal   - Optimum value that minimises the multivariate function
+
+
+end 
