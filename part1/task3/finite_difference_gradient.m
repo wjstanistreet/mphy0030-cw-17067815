c68422cacb207fc6919a818bc9e6daf7adeb4715
Will Stanistreet <zcemwjs@ucl.ac.uk>
2021-01-30 22:52:12 +0800

    Added QP and FDG functions

---
+++ part1/task3/finite_difference_gradient.m
@@ -0,0 +1,16 @@
+function fdg = finite_difference_gradient(mvf, ivv)
+%  
+% finite_difference_gradient calculates the finite difference between
+% points in a multivariate function, mvf. 
+% 
+% --- Inputs ---
+% 
+% mvf       - Multivariate input function 
+% ivv       - Input variable vector 
+% 
+% --- Output ---
+% 
+% fdg       - Finite difference gradient
+
+
+end 
---
+++ part1/task3/quadratic_polynomial.m
@@ -0,0 +1,17 @@
+function qpOut = quadratic_polynomial(a, x)
+% 
+% quadratic_polynomial computes a 2nd degree multivariate polynomial.
+% 
+% --- Inputs ---
+% 
+% a         -  Input parameter vector
+% x         -  Variable vector
+% 
+% --- Output ---
+% 
+% qpOut     - Scalar quadratic polynomial output
+
+
+
+
+end
