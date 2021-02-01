5f67beb988a495fd007d0cf561fcded1f657e056
Will Stanistreet <zcemwjs@ucl.ac.uk>
2021-02-02 04:30:32 +0800

    Initialised task script to read example data

---
+++ part1/task4/task.m
@@ -0,0 +1,10 @@
+% Task 4 script
+
+% Reads the example vartice and triangle data 
+vert = readmatrix('data/example_vertices.csv');
+tri = readmatrix('data/example_triangles.csv');
+
+% Sets default parameters
+lambda = 0.9;
+mu = -1.02*lambda;
+
