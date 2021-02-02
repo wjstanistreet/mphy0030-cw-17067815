631a566f3cb4077e4c52a10e59016681534a2b38
Will Stanistreet <zcemwjs@ucl.ac.uk>
2021-02-02 23:20:24 +0800

    Initialised eval and KG functions

--- part2/RBFSpline.m
+++ part2/RBFSpline.m
@@ -8,18 +8,52 @@ classdef RBFSpline
     
     
     methods 
-        function alpha = fit(p, q, lambda)
-            % Fits a spline based on the initial points 
+        function alpha = fit(p, q, lambda, sigma)
+            % Fits a spline 
             %
             % --- Inputs ---
             % p      - Initial source points
             % q      - Transformed target points
             % lambda - General weighting parameter for localisation errors
+            % sigma  - Individual weights for localisation error
             %
             % --- Output ---
             % alpha - Spline coefficients representing the fitted spline
             
+            
+
         end
+        
+        function queT = evaluate(queP, ctrlP, alpha, sigma)
+            % outputs the kernel values between the query and control
+            % point sets.
+            %
+            % --- Inputs ---
+            % queP   - Initial source points
+            % ctrlP  - Transformed target points
+            % lambda - General weighting parameter for localisation errors
+            % sigma  - Individual weights for localisation error
+            %
+            % --- Output ---
+            % queT   - Transformed query points
+            
+            
+        end  
+        
+        function kernel_gaussian(queP, ctrlP, sigma)
+            % outputs the kernel values between the query and control
+            % point sets
+            %
+            % --- Inputs ---
+            % queP   - Query points to transform to target space
+            % ctrlP  - Control points
+            % sigma  - Individual weights for localisation error
+            %
+            % --- Output ---
+            % alpha - Spline coefficients representing the fitted spline
+            
+            
+        end    
     end
     
 end
