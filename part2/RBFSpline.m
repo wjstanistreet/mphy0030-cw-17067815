999640b06bb1eb83b2ad546d7af0b27946c3a234
Will Stanistreet <zcemwjs@ucl.ac.uk>
2021-02-02 22:37:15 +0800

    Intitialised fit function

---
+++ part2/RBFSpline.m
@@ -0,0 +1,25 @@
+% class RBFSpline - Radial basis function spline for registration
+
+classdef RBFSpline
+    
+    properties
+        
+    end
+    
+    
+    methods 
+        function alpha = fit(p, q, lambda)
+            % Fits a spline based on the initial points 
+            %
+            % --- Inputs ---
+            % p      - Initial source points
+            % q      - Transformed target points
+            % lambda - General weighting parameter for localisation errors
+            %
+            % --- Output ---
+            % alpha - Spline coefficients representing the fitted spline
+            
+        end
+    end
+    
+end
