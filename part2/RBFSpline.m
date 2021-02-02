0e8cbfe3fa026484995dda125c2ce20fa2e2ac50
Will Stanistreet <zcemwjs@ucl.ac.uk>
2021-02-03 03:24:12 +0800

    Added code for fit function

--- part2/RBFSpline.m
+++ part2/RBFSpline.m
@@ -8,7 +8,7 @@ classdef RBFSpline
     
     
     methods 
-        function alpha = fit(p, q, lambda, sigma)
+        function [alpha] = fit(p, q, lambda, sigma)
             % Fits a spline 
             %
             % --- Inputs ---
@@ -20,8 +20,34 @@ classdef RBFSpline
             % --- Output ---
             % alpha - Spline coefficients representing the fitted spline
             
+            % Calculates weighting parameter matrix
+            W = diag(1/sigma^2);
+            Winv = inv(W);
             
-
+            % Initialises Euclidian distances and kernel values for each
+            % dimension x, y, and z.
+            r = zeros(size(p,2));
+            K = zeros(size(p,2));
+            
+            % Calculates the submatrix, Kij = R(||pi - qj||)
+            for i = 1:size(p,2)
+                % Finds the Euclidian distance between points for each
+                % dimension
+                r(:,i) = abs(p(:,i)-q(:,i));
+                K(:,i) = exp((r(:,i)^2)/(2*sigma^2));
+            end
+            
+            % Initialises left hand-size and alpha matrices for each
+            % dimension x, y, and z.
+            LHS = zeros(size(p(1,:)));
+            alpha = zeros(size(p(1,:)));
+            
+            % Calculates alpha, alpha = qk * inv(LHS)
+            for i = 1:size(p,2)
+                % Calculates the LHS of eqn. 3.
+                LHS(i) = K(:,i) + lambda*Winv;
+                alpha(i,:) = inv(LHS(i))*q(i);
+            end
         end
         
         function queT = evaluate(queP, ctrlP, alpha, sigma)
@@ -50,6 +76,9 @@ classdef RBFSpline
             %
             % --- Output ---
             % K      - Kernel values between query and control points
+            
+            
+            
             
             
         end    
