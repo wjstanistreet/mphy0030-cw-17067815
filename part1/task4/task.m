fd1852137b242608081487615f6509cc9421cbb9
Will Stanistreet <zcemwjs@ucl.ac.uk>
2021-02-04 01:35:22 +0800

    Solved 2nd bullet point

--- part1/task4/task.m
+++ part1/task4/task.m
@@ -1,4 +1,5 @@
 % Task 4 script
+clear
 
 % Reads the example vartice and triangle data 
 vert = readmatrix('data/example_vertices.csv');
@@ -8,3 +9,7 @@ tri = readmatrix('data/example_triangles.csv');
 lambda = 0.9;
 mu = -1.02*lambda;
 
+% Smooth each mesh at 5, 10, 25 iterations
+p5  = lowpass_mesh_smoothing(vert, tri, 5);
+p10 = lowpass_mesh_smoothing(vert, tri, 10);
+p25 = lowpass_mesh_smoothing(vert, tri, 25);
