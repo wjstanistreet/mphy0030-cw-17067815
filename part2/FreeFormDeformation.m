d7b27fe3436705f698de505fb8534ef2bc4d2517
Will Stanistreet <zcemwjs@ucl.ac.uk>
2021-02-04 07:28:33 +0800

    Set up optional class constructor

--- part2/FreeFormDeformation.m
+++ part2/FreeFormDeformation.m
@@ -23,16 +23,35 @@ classdef FreeFormDeformation
             defor.z = linspace(range(1,3), range(2,3), nctrlP(3));
             
             % Creates meshgrid 
-            [xMesh, yMesh, zMesh] = meshgrid(defor.x, defor.y, defor.z);
+            [xMeshRG, yMeshRG, zMeshRG] = meshgrid(defor.x, defor.y, defor.z);
+            
             
             % Sets the grid values
-            defor.xMesh = xMesh;
-            defor.yMesh = yMesh;
-            defor.zMesh = zMesh;
+            defor.xMesh = xMeshRG;
+            defor.yMesh = yMeshRG;
+            defor.zMesh = zMeshRG;
+            
+            % Sets the control values described by the mesh coordinates
+            defor.ctrl = [defor.xMesh, defor.yMesh, defor.zMesh];
+            
         end
             
-        function 
+        function deforC = FreeFormDeformationC(nctrlP, Image3D)
             
+             deforC.x = linspace(Image3D.min(1), Image3D.max(2), nctrlP(1));
+             deforC.y = linspace(Image3D.min(2), Image3D.max(2), nctrlP(2));
+             deforC.z = linspace(Image3D.min(3), Image3D.max(3), nctrlP(3));
+            
+             % Creates meshgrid 
+             [xMeshIMG, yMeshIMG, zMeshIMG] = meshgrid(deforC.x, deforC.y, deforC.z);
+            
+             % Sets the grid values
+             deforC.xMesh = xMeshIMG;
+             deforC.yMesh = yMeshIMG;
+             deforC.zMesh = zMeshIMG;
+                    
+             % Sets the control values described by the mesh coordinates
+             deforC.ctrl = [deforC.xMesh, deforC.yMesh, deforC.zMesh];
             
         end
         
