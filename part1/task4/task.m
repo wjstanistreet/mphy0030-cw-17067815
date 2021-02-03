e8a573fa22f5746deeb2a742a7bcd2762aed5f4c
Will Stanistreet <zcemwjs@ucl.ac.uk>
2021-02-04 01:43:02 +0800

    3rd bullet point - needs editing

--- part1/task4/task.m
+++ part1/task4/task.m
@@ -10,6 +10,23 @@ lambda = 0.9;
 mu = -1.02*lambda;
 
 % Smooth each mesh at 5, 10, 25 iterations
-p5  = lowpass_mesh_smoothing(vert, tri, 5);
-p10 = lowpass_mesh_smoothing(vert, tri, 10);
-p25 = lowpass_mesh_smoothing(vert, tri, 25);
+p5  = lowpass_mesh_smoothing(vert, tri, 5, lambda, mu);
+p10 = lowpass_mesh_smoothing(vert, tri, 10, lambda, mu);
+p25 = lowpass_mesh_smoothing(vert, tri, 25, lambda, mu);
+
+% Plots the surface meshes
+figure(1)
+trisurf(tri, vert(:,1), vert(:,2), vert(:,3))
+saveas(figure(1),'Mesh with no smoothing')
+
+figure(2)
+trisurf(tri, p5(:,1), p5(:,2), p5(:,3))
+saveas(figure(2),'Mesh with 5 iterations of smoothing')
+
+figure(3)
+trisurf(tri, p10(:,1), p10(:,2), p10(:,3))
+saveas(figure(3),'Mesh with 10 iterations of smoothing')
+
+figure(4)
+trisurf(tri, p25(:,1), p25(:,2), p25(:,3))
+saveas(figure(4),'Mesh with 25 iterations of smoothing')
