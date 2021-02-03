86fd0d23cb9901c341c9f35344fbbc076fecd1e5
Will Stanistreet <zcemwjs@ucl.ac.uk>
2021-02-03 21:56:32 +0800

    Completed RBFSpline function and edited report qs

--- part2/RBFSpline.m
+++ part2/RBFSpline.m
@@ -8,7 +8,7 @@ classdef RBFSpline
     
     
     methods 
-        function [alpha] = fit(p, q, lambda, sigma)
+        function [alpha] = fit(p, q, lambda, sigma, K)
             % Fits a spline 
             %
             % --- Inputs ---
@@ -23,26 +23,14 @@ classdef RBFSpline
             % Calculates weighting parameter matrix
             W = diag(1/sigma^2);
             Winv = inv(W);
-            
-            % Initialises Euclidian distances and kernel values for each
-            % dimension x, y, and z.
-            r = zeros(size(p,2));
-            K = zeros(size(p,2));
-            
-            % Calculates the submatrix, Kij = R(||pi - qj||)
-            for i = 1:size(p,2)
-                % Finds the Euclidian distance between points for each
-                % dimension
-                r(:,i) = abs(p(:,i)-q(:,i));
-                K(:,i) = exp((r(:,i)^2)/(2*sigma^2));
-            end
-            
+           
             % Initialises left hand-size and alpha matrices for each
             % dimension x, y, and z.
-            LHS = zeros(size(p(1,:)));
-            alpha = zeros(size(p(1,:)));
+            LHS = zeros(size(p));
+            alpha = zeros(size(p));
             
-            % Calculates alpha, alpha = qk * inv(LHS)
+            % Calculates alpha, alpha = qk * inv(LHS) as described in
+            % Question 2 of the report, 
             for i = 1:size(p,2)
                 % Calculates the LHS of eqn. 3.
                 LHS(i) = K(:,i) + lambda*Winv;
@@ -50,17 +38,30 @@ classdef RBFSpline
             end
         end
         
-        function queT = evaluate(queP, ctrlP, alpha, sigma)
+        function [q] = evaluate(queP, ctrlP, alpha, sigma)
             % outputs the transformed query point sets
             %
             % --- Inputs ---
-            % queP   - Initial source points
-            % ctrlP  - Transformed target points
-            % lambda - General weighting parameter for localisation errors
-            % sigma  - Individual weights for localisation error
+            % queP   - Query points to be transformed
+            % ctrlP  - Control points
+            % alpha  - Spline coefficients
+            % sigma  - Weighting pararmeters for localisation error
             %
             % --- Output ---
-            % queT   - Transformed query points
+            % q      - Transformed query points
+            %          qi = u(pi) = sum(alpha*R(||pi - qj||) in each
+            %          dimension.
+            
+            % Calculates the kernel values between the query points, queP, and
+            % control points, ctrlP. 
+            K = kernel_gaussian(queP, ctrlP, sigma);
+            
+            % Initialise transformed points
+            q = zeros(size(queP));
+            
+            for i = 1:size(queP,2)
+                q(:,i) = alpha*K(:,i);
+            end
             
             
         end  
@@ -76,10 +77,19 @@ classdef RBFSpline
             %
             % --- Output ---
             % K      - Kernel values between query and control points
+
+            % Initialises Euclidian distances and kernel values for each
+            % dimension x, y, and z.
+            r = zeros(size(queP,2));
+            K = zeros(size(queP,2));
             
-            
-            
-            
+            % Calculates the submatrix, Kij = R(||pi - qj||)
+            for i = 1:size(queP,2)
+                % Finds the Euclidian distance between points for each
+                % dimension
+                r(:,i) = abs(queP(:,i)-ctrlP(:,i));
+                K(:,i) = exp((r(:,i)^2)/(2*sigma^2));
+            end
             
         end    
     end
